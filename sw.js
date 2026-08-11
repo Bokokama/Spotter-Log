ef214ee3c78b677fd3bb5562f8f4e21711b9070a
Bokokama <potoooooooo83+GitHub@gmail.com>
2026-08-11 11:07:16 +0900

    Add icon files to APP_SHELL array in sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,10 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/maskable-192.png",
+  "./icons/maskable-512.png"
 ];
 
 self.addEventListener("install", event => {
